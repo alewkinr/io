87a09da411bb2a5f0e421251936934e0ba4fec7b
Ramil Aleshkin <alewkin@icloud.com>
2020-11-29 08:13:52 +0300

    :sparkles: Added restrictions parsing from DB
    - DAMN BOY, CHECK THIS HUUUUUGE SQL OUT

--- back/legal-service/legal/api/v1/v1_licenses.py
+++ back/legal-service/legal/api/v1/v1_licenses.py
@@ -1,9 +1,11 @@
 from typing import Any
 
 from sqlalchemy.orm import Session
-from legal.api import deps
+
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from legal.api import deps
+from legal.core.errors import InternalServerErr
 from legal.schemas import License
 
 router = APIRouter()
@@ -19,33 +21,24 @@ def search_license(_license: License, db: Session = Depends(deps.get_db)) -> Any
         "search_string": f"{_license.performers}, {_license.phonogram_title}",
     }
     q = """
-            SELECT
-                   ts_rank(
-                       make_tsvector_ru(performers::text, phonogram_title::text), q
-                    ) AS rankk,
-                    *
-            FROM public."phonograms",
-                 to_tsquery(
-                     'russian',
-                     replace(
-                         plainto_tsquery('russian', '(I JUST) DIED IN YOUR ARMS')::text, '&','|')
-                 ) AS q
-            WHERE make_tsvector_ru(performers, phonogram_title) @@  q
-                AND ts_rank(
-                        make_tsvector_ru(performers::text, phonogram_title::text), q
-                    ) > 0.05
-            ORDER BY ts_rank(
-                make_tsvector_ru(performers, phonogram_title), q
-                ) DESC
-            LIMIT 1;
+            SELECT ts_rank(public.make_tsvector_ru(performers::text, phonogram_title::text), q) as rankk,
+            *
+            FROM public.phonograms,to_tsquery(:language,replace(plainto_tsquery(:language, :search_string)::text, '&','|')) as q 
+            where public.make_tsvector_ru(performers, phonogram_title) @@  q
+            and ts_rank(public.make_tsvector_ru(performers::text, phonogram_title::text), q) > 0.05
+            ORDER by ts_rank(public.make_tsvector_ru(performers, phonogram_title), q) desc limit 1 ;
         """
-    # try:
-    #     res = db.execute(q, params)
-    #     for row in res:
-    #         print(row._asdict())
-    # except Exception as err:
-    #     print(err)
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK, content={"check_result": "restricted"}
-    )
+    try:
+        rows = db.execute(q, params)
+        if not rows.returns_rows and rows.rowcount < 1:
+            return JSONResponse(
+                status_code=status.HTTP_204_NO_CONTENT,
+                content={"check_result": "allowed"},
+            )
+        elif rows.returns_rows and rows.rowcount >= 1:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"check_result": "restricted"},
+            )
+    except Exception as err:
+        raise InternalServerErr(f"error to get info from DB: {err}")
